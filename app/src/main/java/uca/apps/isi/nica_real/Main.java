f747ce900ab5952cdf7884d3709b4629252aacc5
Oscar Moreira <oscarmoreira456@outlook.es>
2017-04-17 16:27:19 -0600

    Dinero

---
+++ app/src/main/java/uca/apps/isi/nica_real/Main.java
@@ -0,0 +1,13 @@
+package uca.apps.isi.nica_real;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Oscar on 17/04/2017.
+ */
+
+public class Main extends AppCompatActivity {
+   private final static String TAG = "Main";
+
+
---
+++ app/src/main/java/uca/apps/isi/nica_real/Models/Dinero.java
@@ -0,0 +1,8 @@
+package uca.apps.isi.nica_real.Models;
+
+/**
+ * Created by Oscar on 17/04/2017.
+ */
+
+public class Dinero {
+}
