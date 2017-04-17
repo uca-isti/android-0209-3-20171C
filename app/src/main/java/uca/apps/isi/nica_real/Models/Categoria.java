b5ebeed3e2668569c97ab30ab0bed25c0d08306f
Adriano98 <olivaresadrian98@gmail.com>
2017-04-17 16:18:08 -0600

    Agregado ApiInterface

---
+++ app/src/main/java/uca/apps/isi/nica_real/Models/Categoria.java
@@ -0,0 +1,9 @@
+package uca.apps.isi.nica_real.Models;
+
+/**
+ * Created by isi3 on 17/4/2017.
+ */
+
+public class Categoria {
+
+}
---
+++ app/src/main/java/uca/apps/isi/nica_real/api/ApiInterface.java
@@ -0,0 +1,25 @@
+package uca.apps.isi.nica_real.api;
+
+import retrofit2.http.Body;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import uca.apps.isi.nica_real.Models.Categoria;
+
+
+/**
+ * Created by isi3 on 17/4/2017.
+ */
+
+public class ApiInterface {
+    @GET("categorias")
+    Call<List<Categoria>> getCategorias();
+
+    @POST("categorias")
+    Call<Categoria> createCategoria(@Body Categoria categoria);
+
+}
+
