8acbbe18a6e68ed1b13fa6458a4f7bf208ee6acb
Saul Somarriba <somarribasaul@gmail.com>
2017-04-17 17:02:22 -0600

    Agregados los atributos de categoria

--- app/src/main/java/uca/apps/isi/nica_real/Models/Categoria.java
+++ app/src/main/java/uca/apps/isi/nica_real/Models/Categoria.java
@@ -6,4 +6,14 @@ package uca.apps.isi.nica_real.Models;
 
 public class Categoria {
 
+    private String nombreCategoria;
+
+    public String getNombreCategoria() {
+        return text;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
 }
