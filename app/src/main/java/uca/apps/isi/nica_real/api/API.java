8d59408f17195340a0bf4c5144368bd0cd8db590
Reinerio <draenwing@gmail.com>
2017-04-17 16:05:11 -0600

    Agregado Api.java

---
+++ app/src/main/java/uca/apps/isi/nica_real/api/API.java
@@ -0,0 +1,13 @@
+package uca.apps.isi.nica_real.api;
+
+/**
+ * Created by isi3 on 17/4/2017.
+ */
+
+public class API {
+    private final static String URL = "https://api-0209-3.herokuapp.com/api";
+
+    public static String getBase() {
+        return URL + "/";
+    }
+}
